00b6ef5a882a51fa1d2a926bc3b5255250f4c728
Serg Nakovnick <w8smile@gmail.com>
2024-06-21 21:45:39 +0300

    add selectors in slice

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,7 @@ import { TodolistsList } from "features/TodolistsList/TodolistsList"
 import { ErrorSnackbar } from "components/ErrorSnackbar/ErrorSnackbar"
 import { useDispatch, useSelector } from "react-redux"
 import { AppRootStateType } from "./store"
-import { initializeAppTC, RequestStatusType } from "./app-reducer"
+import {initializeAppTC, RequestStatusType, selectIsInitialized, selectStatus} from "./app-reducer"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Login } from "features/Login/Login"
 import { logoutTC } from "features/Login/auth-reducer"
@@ -25,8 +25,10 @@ type PropsType = {
 }
 
 function App({ demo = false }: PropsType) {
-  const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
-  const isInitialized = useSelector<AppRootStateType, boolean>((state) => state.app.isInitialized)
+  // const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
+  // const isInitialized = useSelector<AppRootStateType, boolean>((state) => state.app.isInitialized)
+  const status = useSelector(selectStatus)
+  const isInitialized = useSelector(selectIsInitialized)
   const isLoggedIn = useSelector<AppRootStateType, boolean>((state) => state.auth.isLoggedIn)
   const dispatch = useDispatch<any>()
 
--- src/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -3,14 +3,15 @@ import { useDispatch, useSelector } from "react-redux"
 import { AppRootStateType } from "app/store"
 import { AlertProps, Snackbar } from "@mui/material"
 import MuiAlert from "@mui/material/Alert"
-import { appActions } from "app/app-reducer"
+import {appActions, selectError} from "app/app-reducer"
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
 })
 
 export function ErrorSnackbar() {
-  const error = useSelector<AppRootStateType, string | null>((state) => state.app.error)
+  // const error = useSelector<AppRootStateType, string | null>((state) => state.app.error)
+  const error = useSelector(selectError)
   const dispatch = useDispatch()
 
   const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
